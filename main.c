0a82e3fd4599b7e98df7aeb4703006624a19833a
Famerates <koskinenjimi@gmail.com>
2023-07-24 18:07:32 +0300

    checking input

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    printf("\nType anything and the program will type it back\n");
-    char inputInitial[32];
-    fgets(inputInitial, 32,stdin);
-    printf("\nYou typed: %s",inputInitial,"\n");
+    printf("\nType password to continue: ");
+    char inputInitial[64];
+    char pass[] = "password";
+    scanf("%64s", inputInitial);
+    int passcheck = strcmp(pass, inputInitial);
+    if (passcheck == 0) {
+        printf("\nPassword is correct\n");
+    }
+    else {
+        printf("\nPassword is incorrect\n");
+    }
+    
+    printf("stopping program");
+    sleep(3);
     return 0;
 }
